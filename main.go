372c621afd64f845123a25a819fd3fdfd3aa3c77
Arda Firdaus Ramadhan <ardafirdausr@gmail.com>
2021-04-26 20:05:54 +0700

    add 'Add' operator

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ardafirdausr/go-calc/operation"
+)
+
+func main() {
+	var operator string
+	var a, b int
+
+	fmt.Println("Enter <operator> <operand 1> <operand 1>")
+	fmt.Println("Available operator: add")
+	_, err := fmt.Scan(&operator, &a, &b)
+	if err != nil {
+		panic("Failed read input")
+	}
+
+	switch operator {
+	case "add":
+		fmt.Println(operation.Add(a, b))
+	}
+}
